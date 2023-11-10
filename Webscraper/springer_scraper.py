717cc0fd9c0ff8536cedb84b3fdc1b6b17b046ee
jeevanp03 <parmar.jeevan03@gmail.com>
2023-11-10 13:15:33 -0500

    added pdf download, 3 download out of sample of 10

--- Webscraper/springer_scraper.py
+++ Webscraper/springer_scraper.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import requests
 import json
+from bs4 import BeautifulSoup
 load_dotenv()
 
 
@@ -13,7 +14,7 @@ class SpringerXrayScraper:
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def search_springer_articles(self, query, max_results=10):
+    def search_springer_articles(self, query, max_results=10, save_folder="/Users/jeevanparmar/Desktop/co_ops/WAT.ai/X-Ray-Tooling/Webscraper/Springer_PDF"):
         """
         Retrieve up to max_results articles retrieved from query and 
         return a list of dictionaries of the metadata along with the full text if available.
@@ -26,7 +27,7 @@ class SpringerXrayScraper:
                 full_text_link = self._get_full_text_link(article)
                 if full_text_link:
                     article['FullText'] = self._fetch_full_article(
-                        full_text_link)
+                        full_text_link, save_folder)
         return article_metadata
 
     def _get_full_text_link(self, article):
@@ -56,15 +57,19 @@ class SpringerXrayScraper:
             print(f"Error performing Springer search: {e}")
         return []
 
-    def _fetch_full_article(self, url, save_folder):
+    def _fetch_full_article(self, url_list, save_folder):
         """
-        Download the PDF of the article from the given URL and save it in the specified folder.
+        Download the PDF of the article from the given URL list and save it in the specified folder.
         """
+        # Extract the URL from the list of URL dictionaries
+        actual_url = next(
+            (link['value'] for link in url_list if link.get('format') == ''), None)
+        if not actual_url:
+            print("No valid URL found in URL list.")
+            return None
+
         try:
-            # Here you need to implement a way to get the direct PDF download link.
-            # This might involve web scraping, for which you could use libraries like BeautifulSoup or Selenium.
-            # For now, this is a placeholder for the actual PDF download link.
-            pdf_url = self._get_pdf_download_link(url)
+            pdf_url = self._get_pdf_download_link(actual_url)
 
             if pdf_url:
                 response = requests.get(pdf_url, stream=True)
@@ -77,10 +82,9 @@ class SpringerXrayScraper:
                 with open(file_path, 'wb') as f:
                     f.write(response.content)
 
-                print(f"PDF downloaded successfully: {file_path}")
+                print(f"PDF downloaded successfully from URL: {pdf_url}")
+                print(f"PDF downloaded successfully to: {file_path}")
                 return file_path
-            else:
-                print(f"No PDF download link found for URL: {url}")
         except Exception as e:
             print(f"Error downloading PDF: {e}")
         return None
@@ -88,9 +92,31 @@ class SpringerXrayScraper:
     def _get_pdf_download_link(self, article_url):
         """
         Scrape the article page to find the PDF download link.
-        You need to implement this method based on the website's structure.
         """
-        # Placeholder implementation, this needs to be replaced with actual scraping logic.
+        try:
+            response = requests.get(article_url)
+            response.raise_for_status()
+
+            soup = BeautifulSoup(response.content, 'html.parser')
+            pdf_link_element = soup.find('span', class_='c-pdf-download__text')
+
+            if pdf_link_element and pdf_link_element.parent and pdf_link_element.parent.has_attr('href'):
+                relative_pdf_url = pdf_link_element.parent['href']
+
+                # Check if the URL is already absolute
+                if relative_pdf_url.startswith(('http', 'https')):
+                    return relative_pdf_url
+
+                # Correctly concatenate the base URL and the relative URL
+                base_url = 'https://link.springer.com'  # Assuming this is the correct base URL
+                if relative_pdf_url.startswith('/'):
+                    return base_url + relative_pdf_url
+                else:
+                    return base_url + '/' + relative_pdf_url
+
+        except Exception as e:
+            print(f"Error while scraping PDF link: {e}")
+
         return None
 
     def search_and_fetch_full_texts(self, query, max_results=10):
