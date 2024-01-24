580f63890f065b2e9337d8ee5b6e8bb85b7f42f6
root <mspearson9@gmail.com>
2024-01-24 15:09:40 -0500

    added basic embedding classes

--- RAG/chat_interface.py
+++ RAG/chat_interface.py
@@ -5,6 +5,7 @@ from langchain.chains.question_answering import load_qa_chain
 from abc import ABC, abstractmethod
 from dotenv import load_dotenv
 import uuid
+import pandas as pd
 
 class Chat(ABC):
     @abstractmethod
@@ -12,13 +13,6 @@ class Chat(ABC):
         """
         run query through the chat to get a response with rag
         """
-        pass
-
-# TODO: create the child classes... not sure where we're at for this + blocked by not being able to run the embedding on windows
-    # see pubmed_embedding.py for inspiration
-class Embedding(ABC):
-    @abstractmethod
-    def get_similar_documents(self, query):
         pass
 
 class Cohere(Chat):
@@ -61,3 +55,78 @@ class OpenAI(Chat):
         chain = load_qa_chain(self.open_llm, chain_type="stuff")
         out = chain.run(input_documents=rag_docs, question=query)
         return out
+
+class Embedding(ABC):
+    @abstractmethod
+    def run_batch_embeddings_ingestion(self):
+        pass
+
+    def __load_chunked_xray_articles_csv(self, chunked_article_csv="RAG/datasets/xray_articles_chunked.csv"):
+        return pd.read_csv(chunked_article_csv)
+
+class Hugging_face_embedding(Embedding):
+    def __init__(self, chunked_article_csv="RAG/datasets/xray_articles_chunked.csv"):
+        self.chunked_article_csv = chunked_article_csv
+
+    def run_batch_embeddings_ingestion(self, num_docs = None):
+        df = self.load_chunked_xray_articles_csv(self.chunked_article_csv)
+        if num_docs == None:
+            new_df = df.copy()
+        else :
+            new_df = df.head(num_docs).copy()
+        chunks = new_df['text'].tolist()
+        # one call per document
+        pub_ids = new_df['PMID'].tolist()
+
+        # create a dictionary of pub_id -> list[chunk]
+        pub_id_to_chunks = {}
+        for i, pub_id in enumerate(pub_ids):
+            if pub_id not in pub_id_to_chunks:
+                pub_id_to_chunks[pub_id] = []
+            pub_id_to_chunks[pub_id].append(chunks[i])
+
+        # do one call per pub_id'
+        embeddings_as_lists = []
+        for pub_id in pub_id_to_chunks:
+            embedding_result = None
+                # Logic for HuggingFace embeddings
+            embedding_result = self.embedding_model.embed_documents(pub_id_to_chunks[pub_id])
+            embeddings_as_lists.extend([list(embedding) for embedding in embedding_result])
+            print("Finished embedding pub_id: ", pub_id)
+
+        new_df['embedding'] = embeddings_as_lists
+        new_df.to_csv("RAG/datasets/xray_articles_with_embeddings2.csv", index=True)
+
+class Openai_embedding(Embedding):
+    def __init__(self, chunked_article_csv="RAG/datasets/xray_articles_chunked.csv"):
+        self.chunked_article_csv = chunked_article_csv
+
+    def run_batch_embeddings_ingestion(self, num_docs = None):
+        df = self.load_chunked_xray_articles_csv(self.chunked_article_csv)
+        if num_docs == None:
+            new_df = df.copy()
+        else :
+            new_df = df.head(num_docs).copy()
+        chunks = new_df['text'].tolist()
+        # one call per document
+        pub_ids = new_df['PMID'].tolist()
+
+        # create a dictionary of pub_id -> list[chunk]
+        pub_id_to_chunks = {}
+        for i, pub_id in enumerate(pub_ids):
+            if pub_id not in pub_id_to_chunks:
+                pub_id_to_chunks[pub_id] = []
+            pub_id_to_chunks[pub_id].append(chunks[i])
+
+        # do one call per pub_id'
+        embeddings_as_lists = []
+        for pub_id in pub_id_to_chunks:
+            embedding_result = None
+                # Logic for HuggingFace embeddings
+            embedding_result = self.embedding_open.aembed_documents(pub_id_to_chunks[pub_id])
+            embeddings_as_lists.extend([list(embedding) for embedding in embedding_result])
+            print("Finished embedding pub_id: ", pub_id)
+
+        new_df['embedding'] = embeddings_as_lists
+        new_df.to_csv("RAG/datasets/xray_articles_with_embeddings2.csv", index=True)
+
