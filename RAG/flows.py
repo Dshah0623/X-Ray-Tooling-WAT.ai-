405cf01d1a99b1502cab873a791328e7f5c9ce66
Ali <alialhamadani72@gmail.com>
2024-02-17 21:13:22 -0500

    updated chat

--- RAG/flows.py
+++ RAG/flows.py
@@ -1,20 +1,26 @@
 from langchain_core.prompts import ChatPromptTemplate
 from enum import Enum
 import json
+import os
 
 class FlowType(Enum):
-  BASE = 0
-  RESTRICTION = 1
-  HEAT_ICE = 2
-  EXPECTATION = 3
+  ROOT = "root"
+  BASE = "base"
+  RESTRICTION = "restriction"
+  HEAT_ICE = "heat_ice"
+  EXPECTATION = "expectation"
 
-with open("flow-templates.json", "r") as f:
+with open(os.path.join(os.path.dirname(__file__),"flow-templates.json"), "r") as f:
   flows_json = json.loads(f.read())
 
 class Flow:
-  flows: list[str] = [template for template in flows_json]
+  flows: map = flows_json
 
   @classmethod
-  def template(cls, question: str, relevant_documents: list[str], flow_type: FlowType):
-    template_filled = cls.flows[flow_type.value].format(question=question, relevant_documents=relevant_documents)
+  def root(cls) -> str:
+    return cls.flows[FlowType.ROOT.value]
+
+  @classmethod
+  def template(cls, injury: str, injury_location:str, flow_type: FlowType):
+    template_filled = cls.flows[flow_type.value].format(injury=injury, location=injury_location)
     return template_filled
